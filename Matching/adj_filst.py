7de3196f30ba557c1f108185a6d2752f50f64c3c
andersonfrailey <anderson.frailey@aei.org>
2017-05-08 11:26:04 -0400

    pep8 fixes

--- Matching/adj_filst.py
+++ Matching/adj_filst.py
@@ -15,18 +15,18 @@ cps_recs['case1'][case1] = 1
 cps_recs['case2'] = np.nan
 cps_recs['case2'][case2] = 1
 np.random.seed(142)
-cps_recs['z1'] = cps_recs['case1'].apply(lambda x: np.random.uniform(0, 1) if x == 1 else x)
+cps_recs['z1'] = cps_recs['case1'].apply(lambda x: np.random.uniform(0, 1)
+                                         if x == 1 else x)
 np.random.seed(1)
-cps_recs['z2'] = cps_recs['case2'].apply(lambda x: np.random.uniform(0, 1) if x == 1 else x)
-
-selected = (cps_recs['z1'] <= 0.84) | (cps_recs['z2'] <= 0.54)  #TODO: check the probability
+cps_recs['z2'] = cps_recs['case2'].apply(lambda x: np.random.uniform(0, 1)
+                                         if x == 1 else x)
+# TODO: check the probability
+selected = (cps_recs['z1'] <= 0.84) | (cps_recs['z2'] <= 0.54)
 cps_recs['filst'][selected] = 1
-cps_recs.drop(['case1','case2'], axis=1, inplace=True)
+cps_recs.drop(['case1', 'case2'], axis=1, inplace=True)
 
 filers = cps_recs.copy()[(cps_recs['filst'] == 1)]
 nonfilers = cps_recs.copy()[(cps_recs['filst'] == 0)]
-
-
 
 filers.to_csv('cpsrets14.csv', index=False)
 nonfilers.to_csv('cpsnonf2014.csv', index=False)
