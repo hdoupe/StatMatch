74ab17fc4ff92cd199dbeade380d44fddcc06156
Henry Doupe <hdoupe@yahoo.com>
2017-05-26 11:03:03 -0400

    Debugged and first pass at a testing script

--- Matching/adj_filst.py
+++ Matching/adj_filst.py
@@ -5,32 +5,43 @@ Input file: CPSRETS2014.csv
 
 # import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def adjfilst(cps_recs):
-    """
-    """
-    # cps_recs = pd.read_csv('CPSRETS2014.csv')
-    cps_recs['case1'] = np.where(((cps_recs['filst'] == 0) &
-                                 (cps_recs['was'] > 0)), 1, 0)
-    cps_recs['case2'] = np.where(((cps_recs['filst'] == 0) &
-                                  (cps_recs['was'] <= 0)), 1, 0)
-    np.random.seed(142)
-    cps_recs['z1'] = cps_recs['case1'].apply(lambda x: np.random.uniform(0, 1)
-                                             if x == 1 else x)
-    np.random.seed(1)
-    cps_recs['z2'] = cps_recs['case2'].apply(lambda x: np.random.uniform(0, 1)
-                                             if x == 1 else x)
-    # TODO: check the probability
-    selected = (cps_recs['z1'] <= 0.84) | (cps_recs['z2'] <= 0.54)
-    # cps_recs['filst'][selected] = 1
-    cps_recs['filst'] = np.where(selected, 1, cps_recs['filst'])
-    cps_recs.drop(['case1', 'case2'], axis=1, inplace=True)
-    cps_recs['cpsseq'] = cps_recs.index + 1
+	"""
+	"""
+	# cps_recs = pd.read_csv('CPSRETS2014.csv')
+	
+	cps_recs['case1'] = np.where(((cps_recs['filst'] == 0) & (cps_recs['was'] > 0)), 1, 0)
+	cps_recs['case2'] = np.where(((cps_recs['filst'] == 0) & (cps_recs['was'] <= 0)), 1, 0)
+	print ("length of case 1", cps_recs['case1'].sum())
+	print ("length of case 2", cps_recs['case2'].sum())
+	
+	np.random.seed(142)	
+	# the first iteration allowed all case1 and case2 records to be selected since
+	#	1) if record was case1 then case2 was set to zero and thus was selected since it is less than 0.84 and 0.54
+	#	2) vice versa but all case1 were set to zero
+	cps_recs['z1'] = cps_recs.apply(lambda row: np.random.uniform(0, 1)
+											 if row['case1'] == 1 else 1 if row['case2'] else 0,
+											 axis = 1)
+	np.random.seed(1)
+	cps_recs['z2'] = cps_recs.apply(lambda row: np.random.uniform(0, 1)
+											 if row['case2'] == 1 else 1 if row['case1'] else 0,
+											 axis = 1)
+											 
+	# TODO: check the probability
+	# Note: the probability of choosing a record in case1 and case 2 is pretty high
+	# (0.926 = 0.84*0.54 + 0.84*0.46 + 0.54*0.16)
+	selected = np.where(((cps_recs['z1'] <= 0.84) | (cps_recs['z2'] <= 0.54)),1,0)
+	# cps_recs['filst'][selected] = 1
+	
+	# this line guarantees that all filst == 1
+	cps_recs['filst'] = np.where(selected, 1, cps_recs['filst'])
+	cps_recs.drop(['case1', 'case2'], axis=1, inplace=True)
+	cps_recs['cpsseq'] = cps_recs.index + 1
 
-    filers = cps_recs.copy()[(cps_recs['filst'] == 1)]
-    nonfilers = cps_recs.copy()[(cps_recs['filst'] == 0)]
-
-    filers.to_csv('cpsrets14.csv', index=False)
-    nonfilers.to_csv('cpsnonf2014.csv', index=False)
-    return filers, nonfilers
+	filers = cps_recs.copy()[(cps_recs['filst'] == 1)]
+	nonfilers = cps_recs.loc[cps_recs['filst'] == 0,:]
+	filers.to_csv('cpsrets14.csv', index=False)
+	nonfilers.to_csv('cpsnonf2014.csv', index=False)
+	return filers, nonfilers
--- Matching/runmatch.py
+++ Matching/runmatch.py
@@ -13,20 +13,24 @@ from add_nonfilers import add_nonfiler
 import pandas as pd
 
 # Create original CPS file
-mar_cps = cpsmar.create_cps('asec2014_pubuse_tax_fix_5x8.dat')
-print 'CPS Created'
+mar_cps = cpsmar.create_cps('asec2014_pubuse_tax_fix_5x8_2017.dat')
+print ('CPS Created')
 rets = Returns(mar_cps)
-cps = rets.computation()
-print 'CPS Tax Units Created'
-filers, nonfilers = adjfilst(cps)
-print 'Adjustment Complete'
+cps = rets.computation() #pd.read_csv('CPSRETS2014.csv')
+print ('CPS Tax Units Created')
+filers, nonfilers = adjfilst(cps) #pd.read_csv('cpsrets14.csv'), pd.read_csv('cpsnonf2014.csv') 
+print ('Adjustment Complete')
 puf = pd.read_csv('puf2009.csv')
 soi = create_soi(puf)
-print 'PUF Created'
+print ('PUF Created')
 soi_final, cps_final, counts = phaseone(filers, soi)
-print 'Start Phase Two'
+print ('Start Phase Two')
 match = phasetwo(soi_final, cps_final)
-print 'Creating final file'
+print ('Creating final file')
 cpsrets = add_cps(filers, match, puf)
+
+# nonfilers = pd.read_csv('cpsnonf2014.csv')
+# cpsrets = pd.read_csv('cpsrets.csv')
+
 cps_matched = add_nonfiler(cpsrets, nonfilers)
 cps_matched.to_csv('cps-matched-puf.csv')
